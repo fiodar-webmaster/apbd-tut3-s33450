791af3fa8667af06f419fa365ca63fc94079a904
fiodar-webmaster <murasakibara314@gmail.com>
2026-03-22 12:22:32 +0100

    Implement RentalPolicy class with general rules

--- Tutorial3/Tutorial3/Logic/RentalPolicy.cs
+++ Tutorial3/Tutorial3/Logic/RentalPolicy.cs
@@ -1,6 +1,8 @@
 ﻿namespace Tutorial3.Logic;
 
-public class RentalPolicy
+public static class RentalPolicy
 {
-    
+    public const int MaxStudentRentals = 2;
+    public const int MaxEmployeeRentals = 5;
+    public const decimal LateFeeDaily = 3;
 }
