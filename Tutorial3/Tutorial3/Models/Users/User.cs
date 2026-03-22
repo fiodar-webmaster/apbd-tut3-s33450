39ca841f6e01ff109f38f6665aceb2f04c78f9f1
fiodar-webmaster <murasakibara314@gmail.com>
2026-03-22 10:52:02 +0100

    Implement User abstract base class

---
+++ Tutorial3/Tutorial3/Models/Users/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Tutorial3.Models.Users;
+
+public abstract class User
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    
+    protected  User(string name, string surname, string email)
+    {
+        Name = name;
+        Surname =  surname;
+        Email = email;
+    }
+}
