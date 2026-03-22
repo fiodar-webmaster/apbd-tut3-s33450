1c492ec1926a4138faa68e5fd10a8339f7cf8909
fiodar-webmaster <murasakibara314@gmail.com>
2026-03-22 11:28:11 +0100

    Implement RentalAct class

---
+++ Tutorial3/Tutorial3/Logic/RentalAct.cs
@@ -0,0 +1,25 @@
+﻿using Tutorial3.Models.Equipment;
+using Tutorial3.Models.Users;
+namespace Tutorial3.Logic;
+
+public class RentalAct
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public User Borrower { get; }
+    public Equipment BorrowedItem{ get; }
+    public DateTime BorrowDate { get; }
+    public DateTime DueDate { get; }
+    
+    public DateTime? ReturnDate { get; set; }
+    public decimal? Fee { get; set; } 
+    
+    public RentalAct(User borrower, Equipment borrowedItem, DateTime borrowDate, DateTime dueDate)
+    {
+        Borrower = borrower;
+        BorrowedItem = borrowedItem;
+        BorrowDate = borrowDate;
+        DueDate = dueDate;
+    }
+
+
+}
