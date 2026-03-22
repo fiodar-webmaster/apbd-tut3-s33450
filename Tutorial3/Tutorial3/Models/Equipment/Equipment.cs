9a4168bb7b87dd13176d2d14ba8ad34b0e2a720a
fiodar-webmaster <murasakibara314@gmail.com>
2026-03-22 10:17:20 +0100

    Create directory structure, implement abstract class Equipment

---
+++ Tutorial3/Tutorial3/Models/Equipment/Equipment.cs
@@ -0,0 +1,13 @@
+﻿namespace Tutorial3.Models.Equipment;
+
+public abstract class Equipment {
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Name { get; set; }
+    public bool IsAvailable { get; set; } = true;
+
+    protected Equipment(string name)
+    {
+        Name = name;
+    }
+     
+}
